b4bb5738f9539e3d3d2a2af2bef6410464ffea2d
paunov0000 <legendarygriefer96@gmail.com>
2024-06-05 11:34:28 +0300

    Imported Calculator component, removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
-	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<p className='text-red-500 bg-white'>Red Text</p>
+			<Calculator></Calculator>
 		</>
 	);
 }
