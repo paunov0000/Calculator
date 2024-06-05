4eafd985428fcfa2062200be6409d5fc7c0a8073
paunov0000 <legendarygriefer96@gmail.com>
2024-06-05 23:59:59 +0300

    Implemented arithmetic operations

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,12 +1,31 @@
 import { parse } from 'postcss';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Keypad({ numbers, onButtonClick }) {
+	const [operator, setOperator] = useState(null);
+	const [sum, setSum] = useState(0);
+
 	const rows = [];
 	let number = 9;
+	const symbols = [
+		<div onClick={(e) => handleOperatorClick(e.currentTarget.textContent)}>
+			<p>*</p>
+		</div>,
+		<div onClick={(e) => handleOperatorClick(e.currentTarget.textContent)}>
+			<p>-</p>
+		</div>,
+		<div onClick={(e) => handleOperatorClick(e.currentTarget.textContent)}>
+			<p>+</p>
+		</div>,
+	];
 	rows.push(
 		<>
-			<div onClick={() => onButtonClick(0)}>
+			<div
+				onClick={() => {
+					setOperator(null);
+					onButtonClick(0);
+				}}
+			>
 				<p>AC</p>
 			</div>
 			<div>
@@ -15,86 +34,76 @@ export default function Keypad({ numbers, onButtonClick }) {
 			<div>
 				<p>%</p>
 			</div>
-			<div>
+			<div onClick={(e) => handleOperatorClick(e.currentTarget.textContent)}>
 				<p>/</p>
 			</div>
 		</>
 	);
-	for (let row = 0; row < 3; row++) {
+	for (let row = 1; row <= 3; row++) {
 		const cols = [];
-		for (let col = 0; col < 2; col++) {
+		let numStartFrom = number - 3 * row;
+		for (let col = 1; col <= 3; col++) {
+			numStartFrom++;
 			cols.push(
-				<div>
-					<p></p>
+				<div onClick={(e) => handleDigitClick(e.currentTarget.textContent)}>
+					<p>{numStartFrom}</p>
 				</div>
 			);
 		}
+		cols.push(symbols[row - 1]);
+		rows.push(cols);
 		//Add the  +-=*/ operations each on the end of the row
 	}
+	rows.push(
+		<div className='col-span-2'>
+			<p>0</p>
+		</div>,
+		<div>
+			<p>.</p>
+		</div>,
+		<div onClick={(e) => handleOperatorClick(e.currentTarget.textContent)}>
+			<p>=</p>
+		</div>
+	);
 
-	function handleOnClick(textContent) {
+	function handleDigitClick(textContent) {
+		console.log(operator);
 		if (numbers !== 0) {
-			return onButtonClick(parseInt(numbers + textContent));
+			onButtonClick(parseInt(numbers + textContent));
 		} else {
 			onButtonClick(parseInt(textContent));
 		}
 	}
 
-	return (
-		<div className='grid grid-cols-4 grid-rows-5'>
-			{rows}
-			<div>
-				<p>7</p>
-			</div>
-			<div>
-				<p>8</p>
-			</div>
-			<div>
-				<p>9</p>
-			</div>
-			<div>
-				<p>*</p>
-			</div>
-			<div>
-				<p>4</p>
-			</div>
-			<div>
-				<p>5</p>
-			</div>
-			<div
-				onClick={(e) => {
-					handleOnClick(e.currentTarget.textContent);
-				}}
-			>
-				<p>6</p>
-			</div>
-			<div>
-				<p>-</p>
-			</div>
-			<div>
-				<p>1</p>
-			</div>
-			<div>
-				<p>2</p>
-			</div>
-			<div>
-				<p>3</p>
-			</div>
-			<div>
-				<p>+</p>
-			</div>
-			<div
-				className='col-span-2'
-				onClick={(e) => handleOnClick(e.currentTarget.textContent)}
-			>
-				<p>0</p>
-			</div>
-			<div>
-				<p>.</p>
-			</div>
-			<div>
-				<p>=</p>
-			</div>
-		</div>
-	);
+	function handleOperatorClick(clickedOperator) {
+		if (clickedOperator !== '=') {
+			setSum(numbers);
+			setOperator(clickedOperator);
+			onButtonClick(0);
+		} else {
+			let totalSum = 0;
+			switch (operator) {
+				case '*':
+					totalSum = sum * numbers;
+					break;
+
+				case '-':
+					totalSum = sum - numbers;
+					break;
+
+				case '/':
+					totalSum = sum / numbers;
+					break;
+				case '+':
+					totalSum = sum + numbers;
+					break;
+				default:
+					break;
+			}
+			setSum(totalSum);
+			onButtonClick(totalSum);
+		}
+	}
+
+	return <div className='grid grid-cols-4 grid-rows-5'>{rows}</div>;
 }
