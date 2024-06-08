5dec946d922548f51cab40cb65f27261148ff315
paunov0000 <legendarygriefer96@gmail.com>
2024-06-08 23:36:36 +0300

    Made the Calculator component a grid

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,7 +7,7 @@ export default function Calculator() {
 	const [numbers, setNumbers] = useState('');
 
 	return (
-		<div className='text-4xl'>
+		<div className='text-3xl grid grid-cols-4 grid-rows-5 gap-2.5'>
 			<Display numbers={numbers}></Display>
 			<Keypad
 				numbers={numbers}
