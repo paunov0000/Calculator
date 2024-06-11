ef72894c46c3be5bfd1b6aedbec31ee648fbc930
paunov0000 <legendarygriefer96@gmail.com>
2024-06-11 21:44:53 +0300

    used custom calculator utilities for grid rows and cols

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,7 +7,7 @@ export default function Calculator() {
 	const [numbers, setNumbers] = useState('');
 
 	return (
-		<div className='text-3xl grid grid-cols-4 grid-rows-5 gap-2.5'>
+		<div className='text-3xl grid grid-cols-calculator grid-rows-calculator gap-2.5'>
 			<Display numbers={numbers}></Display>
 			<Keypad
 				numbers={numbers}
