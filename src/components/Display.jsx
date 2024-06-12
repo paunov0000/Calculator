70d91cf323d8bfe9fd706c0d80d0e4623d1b0937
paunov0000 <legendarygriefer96@gmail.com>
2024-06-12 20:12:00 +0300

    Increased text size

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Display({ numbers }) {
-	return <p className='col-span-4 text-right'>{numbers === '' ? 0 : numbers}</p>;
+	return <p className='col-span-4 text-right text-6xl'>{numbers === '' ? 0 : numbers}</p>;
 }
