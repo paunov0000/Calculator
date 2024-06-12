a4688862e138a784c4799b56c7d74774d9fdfcc1
paunov0000 <legendarygriefer96@gmail.com>
2024-06-12 20:11:39 +0300

    Extracted grid-related classes and passed them to Keypad component. Added spacing between Display and Keypad components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,11 +7,12 @@ export default function Calculator() {
 	const [numbers, setNumbers] = useState('');
 
 	return (
-		<div className='text-3xl grid grid-cols-calculator grid-rows-calculator gap-2.5 font-calculator'>
+		<div className='text-3xl font-calculator flex flex-col gap-3'>
 			<Display numbers={numbers}></Display>
 			<Keypad
 				numbers={numbers}
 				onButtonClick={setNumbers}
+				className='grid grid-cols-calculator grid-rows-calculator gap-2.5 '
 			></Keypad>
 		</div>
 	);
